49a91439ccc5fda730a715e7103259151b8f6375
a-wtnbe <ayawatanabe714@gmail.com>
2021-04-15 07:12:01 +0000

    一通り完成、リンクで手詰まり

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,6 +2,7 @@ class ListController < ApplicationController
   def new
     # からモデル作成
     @book = Book.new
+    @book = Book.all
   end
 
   # 新規投稿
@@ -10,12 +11,28 @@ class ListController < ApplicationController
     book = Book.new(book_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    # ３. トップ画面へリダイレクト
-    # redirect_to '/top'
+    # ３. 詳細へリダイレクト
+    redirect_to list_path(list.ids)
   end
 
-  def new
-    @book = Book.all
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to list_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to list_path
   end
 
   # こっから下はコントローラーの中でしか呼ばないぞ
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # 新規投稿
   post 'list' => 'list#create'
-  # # 一覧
-  get 'list' => 'list#new'
-
+  # 一覧
+  # get 'list' => 'list#new'
   # 詳細設定
-
+  get 'list/:id' => 'list#show', as: 'show_list'
+  # # 編集
+  get 'list/:id/edit' => 'list#edit', as: 'edit_list'
+  # patch 'list/:id' => 'list#update', as: 'update_list'
 end
